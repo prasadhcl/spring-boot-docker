b50c159b570bf72b8855109c598b0593e47493ad
prasadhcl <gandlap@hcl.com>
2016-12-15 11:16:27 +0000

    Update Application.java
    SOP Added for test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-     
+                System.out.println("In Application main method start");
 
 		SpringApplication.run(Application.class, args);
 	}
